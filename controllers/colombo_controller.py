Added point weighting for the spline fitting Changed the UI to show the fixed points

controllers/colombo_controller.py
@@ -42,15 +42,10 @@ class ColomboController(MethodController):
             return {'axis': self.controller.axis, 'vanishing_line': vanishing_line, 'ic':ic, 'jc':jc}
 
     class ContourFiltering(Stage):
-        def _process(self, input):
-            data = self.controller.contour[self.controller.options['filter_mask']]
-            return {**input, 'output': data}
+        def _process(self, stage_data):
+            data = self.controller.contour[self.controller.options['filter_mask']]            
+            axis = stage_data['axis']
 
-    class SplineFittingAndTangent(Stage):
-        def _process(self, input):
-            data, axis = input['output'], input['axis']
-            logger.info(f'Initial number of points. {len(data)}')
-            
             data = data[sort_by_distance_to_axis(data, axis)]
 
             orientation = check_contour_orientation(data)
@@ -59,23 +54,41 @@ class ColomboController(MethodController):
                 data = reflect_points_on_line(data, axis)
                 logger.info('Applied a reflection over the initial points.')
             
+            fixed_idx = set([0, len(data) - 1])
+            for ellipse in self.controller.ellipses[1:]:
+                distances = [ellipse.aprox_distance_to_point(p) for p in data]
+                fixed_idx.add(np.argmin(distances))
+            fixed_points = np.array([data[i] for i in fixed_idx])
+
+            return {**stage_data, 'output': data, 'fixed_idx': fixed_idx, 'fixed_points': fixed_points, 'top_point': data[0]}
+
+    class SplineFittingAndTangent(Stage):
+        def _process(self, stage_data):
+            data, axis = stage_data['output'], stage_data['axis']
+            logger.info(f'Initial number of points. {len(data)}')
+            
+            fixed_idx = stage_data['fixed_idx']
+
             parametrization = self.controller.options['parametrization']
             s = self.controller.options['smoothing_factor'] * len(data)
             t = np.linspace(0.0, 1.0, self.controller.options['sample_size'])
             logger.info(f'Smoothing Factor {s}.')
 
-            data, tangent_vectors = generate_and_evaluate_spline(data, s=s, t=t, parametrization=parametrization)
+            weights = np.ones(len(data))
+            weights[[*fixed_idx]] = 10 ** 6
+            
+            data, tangent_vectors = generate_and_evaluate_spline(data, s=s, t=t, parametrization=parametrization, weights=weights)
             data = np.c_[*data, np.ones_like(t)]
+            logger.info(f'Spline fit to contour and sampled. Number of sample points: {len(data)}')
+
             tangent_vectors = np.c_[*tangent_vectors, np.zeros_like(t)]
             tangent_vectors /= np.linalg.norm(tangent_vectors, axis=1)[:, np.newaxis]
-
-            logger.info(f'Spline fitted to contour and sampled. Number of sample points: {len(data)}')
 
             tangent_lines = np.cross(data, tangent_vectors)
             tangent_lines /= np.linalg.norm(tangent_lines[:, :2], axis=1)[:, np.newaxis]
 
             theta = get_angle_of_line(axis)
-            if np.abs(theta - np.pi / 2) > 1e-10:
+            if not self.controller.options['disable_filter_overall'] and np.abs(theta - np.pi / 2) > 1e-10:
                 diff = theta - np.pi / 2 if theta > 0 else theta + np.pi / 2
                 rotation_matrix_z = rotation_matrix('z', -diff)
 
@@ -86,16 +99,16 @@ class ColomboController(MethodController):
                 normals_rectify /= np.linalg.norm(normals_rectify, axis=1)[:, np.newaxis]
                 n_x, n_y = normals_rectify[:, 0], normals_rectify[:, 1]
 
-                if not self.controller.options['disable_filter_overall']:
-                    mask_values = np.abs(np.divide(n_y, n_x, where=n_x != 0))
-                    data, tangent_lines = filter_outliers(data, tangent_lines, mask_values=mask_values, m=self.controller.options['filter_outliers'], msg='Outliers in normals.')
+                mask_values = np.abs(np.divide(n_y, n_x, where=n_x != 0))
+                data, tangent_lines = filter_outliers(data, tangent_lines, mask_values=mask_values,
+                                                      m=self.controller.options['filter_outliers'], msg='Outliers in normals.')
 
-            return {**input, 'output': data, 'tangent_lines': tangent_lines}
+            return {**stage_data, 'output': data, 'tangent_lines': tangent_lines}
 
     class HomologyAndCrossRatio(Stage):
-        def _process(self, input):
-            data, tangent_lines = input['output'], input['tangent_lines']
-            axis, vanishing_line = input['axis'], input['vanishing_line']
+        def _process(self, stage_data):
+            data, tangent_lines = stage_data['output'], stage_data['tangent_lines']
+            axis, vanishing_line = stage_data['axis'], stage_data['vanishing_line']
 
             w_homologies, w_chars, _, _ = generate_w_homologies(data, tangent_lines, axis, vanishing_line, self.controller.ellipse_base_matrix)
 
@@ -107,12 +120,12 @@ class ColomboController(MethodController):
                 data, w_homologies, w_chars = filter_outliers(data, w_homologies, w_chars, mask=filter_data_by_percentages(w_chars, *self.controller.options['filter_cross_ratio']),
                                                               msg='Cross-Ratio.', user=True)
 
-            return {**input, 'output': data, 'w_homologies': w_homologies, 'cross-ratio': w_chars}
+            return {**stage_data, 'output': data, 'w_homologies': w_homologies, 'cross-ratio': w_chars}
     
     class RectificationAndNormalization(Stage):
-        def _process(self, input):
-            axis, vanishing_line = input['axis'], input['vanishing_line']
-            w_homologies, cross_ratio = input['w_homologies'], input['cross-ratio']
+        def _process(self, stage_data):
+            axis, vanishing_line = stage_data['axis'], stage_data['vanishing_line']
+            w_homologies, cross_ratio = stage_data['w_homologies'], stage_data['cross-ratio']
 
             logger.trace('Searching for a good ellipse point that will be part of the unrectified meridian.')
             if not self.controller.options['user_ellipse']:
@@ -143,7 +156,7 @@ class ColomboController(MethodController):
                         cos_t = (A * F - B * D) / denom_cos
                         sin_t = (B * C - A * E) / denom_sin
                         return np.arctan2(sin_t, cos_t)
-                    if np.sign(np.dot(axis, p)) == np.sign(np.dot(axis, input['output'][0])):
+                    if np.sign(np.dot(axis, p)) == np.sign(np.dot(axis, stage_data['output'][0])):
                         phi = find_t(p[0], p[1], self.controller.ellipse_base)
                         phi = np.pi if np.isnan(phi) else phi
                     else:
@@ -184,7 +197,7 @@ class ColomboController(MethodController):
                 unrectified_meridian = unrectified_meridian[sorted_idx]
                 cross_ratio = cross_ratio[sorted_idx]
             
-            return {**input, 'output': normalized_data, 'w_homologies': w_homologies, 'unrectified_meridian': unrectified_meridian, 'cross-ratio':cross_ratio}
+            return {**stage_data, 'output': normalized_data, 'w_homologies': w_homologies, 'unrectified_meridian': unrectified_meridian, 'cross-ratio':cross_ratio}
  
     def __init__(self, data_controller: DataController, identifier):
         stages = [self.EntitySelection(self, pause=True),

controllers/kwan_controller.py
@@ -49,22 +49,28 @@ class KwanController(MethodController):
             transformation = rot_a @ rot_b @ np.linalg.inv(self.controller.camera_matrix)
             data = apply_transformation(transformation, data)
 
+            data[:, 0] = np.abs(data[:, 0])
+            data = data[sort_by_distance_to_axis(data, [1, 0, 0])]
+
+            fixed_idx = set([0, len(data) - 1])
             ellipses_coefficients = []
             for ellipse in self.controller.ellipses:
                 ei = dataclasses.replace(ellipse)
                 ei.apply_transformation(transformation)
                 a, _, c, _, e, f = ei.get_pol_to_cart()
                 ellipses_coefficients.append((a, c, e, f))
+                distances = [ei.aprox_distance_to_point(p) for p in data]
+                fixed_idx.add(np.argmin(distances))
 
             logger.info(f'Looking for rotation angle using {len(self.controller.ellipses)} ellipses.')
             result = search_angle_rotation_x(ellipses_coefficients)
             rotation_angle_x = result.x[0]
             logger.trace(f'Angle opt result. {result.message}')
 
-            data[:, 0] = np.abs(data[:, 0])
-            data = data[sort_by_distance_to_axis(data, [1, 0, 0])]
+            fixed_points = np.array([data[i] for i in fixed_idx])
 
-            return {**stage_data, 'output': data, 'rotation_angle_x': rotation_angle_x}
+            return {**stage_data, 'output': data, 'rotation_angle_x': rotation_angle_x,
+                    'fixed_idx': fixed_idx, 'fixed_points': fixed_points}
         
     class SplineFittingAndNormal(Stage):
         def _process(self, stage_data):
@@ -74,17 +80,20 @@ class KwanController(MethodController):
             s = self.controller.options['smoothing_factor'] * len(data)
             t = np.linspace(0.0, 1.0, self.controller.options['sample_size'])
 
-            data, tangent_vectors = generate_and_evaluate_spline(data, s=s, t=t, parametrization=parametrization)
+            fixed_idx = stage_data['fixed_idx']
+            weights = np.ones(len(data))
+            weights[[*fixed_idx]] = 10 ** 6
+
+            data, tangent_vectors = generate_and_evaluate_spline(data, s=s, t=t, parametrization=parametrization, weights=weights)
             data = np.c_[*data, np.ones_like(t)]
             tangent_vectors = np.c_[*tangent_vectors, np.zeros_like(t)]
             tangent_vectors /= np.linalg.norm(tangent_vectors, axis=1)[:, np.newaxis]
 
-            logger.info(f'Spline fitted to contour and sampled. Number of sample points: {len(data)}')
+            logger.info(f'Spline fit to contour and sampled. Number of sample points: {len(data)}')
 
             normals = np.cross(data, tangent_vectors)
             normals /= np.linalg.norm(normals, axis=1)[:, np.newaxis]
                 
-
             if not self.controller.options['disable_filter_overall']:
                 n_x, n_y = normals[:, :2].T
                 mask_values = np.abs(np.divide(n_y, n_x, where=n_x != 0))

shared/dataclasses_.py
@@ -47,6 +47,14 @@ class _Ellipse:
     def get_points(self, npts=100, tmin=0.0, tmax=2*np.pi):
         return get_ellipse_pts((*self.center, *self.axes, self.angle), npts, tmin, tmax)
 
+    def aprox_distance_to_point(self, x): # Sampson approximation to the geometric distance
+        C = self.get_matrix_form()
+        Cx = C @ x
+        numerator = np.dot(x, Cx) ** 2
+        a, b = Cx[0], Cx[1]
+        denominator = 4.0 * (a ** 2 + b ** 2)
+        return numerator / denominator
+
     def __post_init__(self):
         if isinstance(self.center, list) and isinstance(self.axes, list):
             self.center = tuple(self.center)
@@ -54,8 +62,7 @@ class _Ellipse:
 
     def __str__(self):
         a, b, c, d, e, f = self.get_pol_to_cart()
-        print(f'{a:.15f} x^2 + {b:.15f} x y + {c:.15f} y^2 + {d:.15f} x + {e:.15f} y + {f:.15f} = 0')
-
+        return f'{a:.15f} x^2 + {b:.15f} x y + {c:.15f} y^2 + {d:.15f} x + {e:.15f} y + {f:.15f} = 0'
 
 @dataclass
 class ImageMetadata:

shared/optimization.py
@@ -24,7 +24,7 @@ def sort_by_distance_to_axis(points, axis):
     max_distance = max(np.mean(points[:, :2], axis=1)) * 0.3
     return sort_by_distance(points, idx, max_distance=max_distance)
 
-def generate_and_evaluate_spline(points, t, k=3, s=0, parametrization='chord'):
+def generate_and_evaluate_spline(points, t, k=3, s=0, parametrization='chord', weights=None):
     x, y = points[:, :2].T
     p = np.column_stack((x, y))
 
@@ -45,7 +45,12 @@ def generate_and_evaluate_spline(points, t, k=3, s=0, parametrization='chord'):
     u = get_u_parametrization()
     u /= u.max()
     
-    tck, _ = splprep([x, y], u=u, k=k, s=s)
+
+    if weights is None:
+        tck, _ = splprep([x, y], u=u, k=k, s=s)
+    else:
+        tck, _ = splprep([x, y], w=weights, u=u, k=k, s=s)
+
     return splev(t, tck), splev(t, tck, der=1)
 
 def generate_spline(points, k=3, parametrization='chord', bc_type='natural'):
@@ -74,6 +79,19 @@ def generate_spline(points, k=3, parametrization='chord', bc_type='natural'):
     if k == 1:
         bc_type = None
     return make_interp_spline(t, np.c_[xn, yn], k=k, bc_type=bc_type)
+
+def normalize_range2(points, xrange, yrange):
+    xmin, xmax = xrange
+    ymin, ymax = yrange
+
+    xdiff, ydiff = xmax-xmin, ymax-ymin
+
+    xyratio = xdiff / ydiff
+    
+    xs = np.array([(float(i) - xmin) / xdiff * xyratio for i in points[:, 0]])
+    ys = np.array([(float(i) - ymin) / ydiff for i in points[:, 1]])
+    
+    return np.column_stack((xs, ys, np.ones_like(xs)))
 
 def normalize_range(points, xmin=None):
     if xmin is None: